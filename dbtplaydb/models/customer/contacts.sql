8e46d62ccc0877854d475faff65bf73289e2506a
Anton Pious Alfred <antonpious@gmail.com>
2025-07-23 23:03:33 -0400

    Added incremental mode

---
+++ dbtplaydb/models/customer/contacts.sql
@@ -0,0 +1,41 @@
+{{ config(
+  post_hook = "
+update {{this}}
+ set is_active = 'false'
+ where contact_id
+ in
+ (select t.contact_id from
+{{ this }} t
+ left join {{ source('dbtplaydb', 'contacts')}} s
+  on t.contact_id = s.id
+ where s.id is null)"
+) }}
+
+with
+source as (
+
+    select * from {{ source('dbtplaydb', 'contacts')}}
+    {% if is_incremental() %}
+        where updated_at > (SELECT MAX(updated_at) FROM {{ this }})  -- incremental condition
+    {% endif %}
+),
+
+contacts as (
+    select 
+        ----- id
+        id as contact_id,
+        ----- full name
+        full_name,
+        --- email
+        email,
+        --- created at
+        created_at,
+        --- updated at
+        updated_at,
+        -- last loaded as current timestamp,
+        true as is_active,
+        CURRENT_TIMESTAMP as last_loaded_at
+    from source
+)
+
+select * from contacts
